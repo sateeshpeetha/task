da86bf18e94cdccceb10fc5457cfe2802390f376
Sateesh Peetha <sateesh_peetha@yahoo.com>
2017-06-08 14:09:41 +0530

    Create task.js

--- task.js
+++ task.js
@@ -151,7 +151,7 @@ function TaskCtrl($scope, $element, $attrs) {
 
   $scope.editUpdate = function(task) {
 
-    console.log(task);
+    console.log(task,event,e);
 
     if (event.which == 13 && task.x.taskText !== "") {
       $scope.isUpdDuplicate(task.x, function(dup) {
